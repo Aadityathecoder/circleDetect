426575616a9e54a8b1ac7f692e5a208065f4dd15
Aadityathecoder <102342120+Aadityathecoder@users.noreply.github.com>
2026-01-09 08:59:14 -0500

    Update soda.py

--- soda.py
+++ soda.py
@@ -27,13 +27,11 @@ imgreading():
 """
 def imgreading(path):
     img = cv.imread("sodaCircle.png")
-    if img is None:
-        raise FileNotFoundError(f"Image not found")
     return img
 """
 Parameters:
     gray (np.ndarray): Grayscale version of the input image. 
-    width (int): Width of the original image (used to scale radius bounds).
+    width (int): Width of the original image.
 Returns:
     tuple[int, int, int]: (x, y, r) center coordinates and radius of the detected circle. This uses the circle equation (x-h)^2 + (y-k)^2 = r^2
 """
@@ -42,10 +40,10 @@ def detect_circle(gray: np.ndarray, width: int):
         cv.GaussianBlur(gray, (9, 9), 2),cv.HOUGH_GRADIENT,dp=1.2,minDist=400,param1=100,param2=50,minRadius=int(width * 0.42),maxRadius=int(width * 0.52))
 
     rounded_circles = np.uint16(np.around(circles))
-    circle_candidates = rounded_circles[0]
+    circle_possible = rounded_circles[0]
     def retrieval(circle):
         return circle[2]
-    largest_circle = max(circle_candidates, key=retrieval)
+    largest_circle = max(circle_possible, key=retrieval)
 
     x, y, r = largest_circle
     return int(x), int(y), int(r)
@@ -65,10 +63,10 @@ def main():
         return
 
     x, y, r = result
-    cv.circle(canister, (x, y), r, (0, 255, 0), 5) #parentheses is the rgb color for both lines
+    cv.circle(canister, (x, y), r, (0, 255, 0), 5) #Parentheses is the rgb color for both lines
     cv.circle(canister, (x, y), 5, (0, 0, 255), 9)
 
-# this is the plt (matplotlib section) of my code, other is CV upwards
+# This is the plt (matplotlib section) of my code, other is CV upwards.
     rgb = cv.cvtColor(canister, cv.COLOR_BGR2RGB)
     plt.figure(figsize=(12, 12))
     plt.imshow(rgb)
